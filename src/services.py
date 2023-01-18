070e79b0c68e6960cd6e81e7ce29ddd7b98e271a
Melissa Farrell <caretcake@outlook.com>
2023-01-18 17:16:13 -0500

    Implement transpose_matrix

--- src/services.py
+++ src/services.py
@@ -1,2 +1,11 @@
 def transpose_matrix(matrix):
-    return [0]
+
+    row_count = len(matrix)
+    col_count = len(matrix[0])
+    transposed_matrix = [[] for i in range(col_count)]
+
+    for current_column in range(col_count):
+        for current_row in range(row_count):
+            transposed_matrix[current_column].append(matrix[current_row][current_column])
+
+    return transposed_matrix
